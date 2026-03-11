cf925153383679dfa77c6254da187eb497a0f23a
Xiangyu Jia <3094969j@student.gla.ac.uk>
2026-03-11 23:04:28 +0000

    Finished the Events page, updated event details and event images

--- student_event_hub/urls.py
+++ student_event_hub/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='base.html'), name='home'),
+    path('', TemplateView.as_view(template_name='Events.html'), name='home'),
     path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
 ]
